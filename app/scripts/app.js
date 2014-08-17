11e77c0be573e0621c391ddbd64283df67ffd269
EC2 Default User <ec2-user@ip-172-31-46-211.us-west-2.compute.internal>
2014-08-17 17:22:01 +0000

    initial

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name rwsprojectApp
+ * @description
+ * # rwsprojectApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('rwsprojectApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rwsprojectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rwsprojectApp
+ */
+angular.module('rwsprojectApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
