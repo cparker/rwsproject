3a72c742b6b6918b1adafc1c5039a9343558ad41
Christian Parker <christian.parker@emc.com>
2014-08-22 16:52:05 -0600

    Switched to bootstrap 2.3.2 from version 3 because version 2.3.2 has left-hand tabs

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -4,16 +4,12 @@
  * @ngdoc function
  * @name rwsprojectApp.controller:MainCtrl
  * @description
- * # MainCtrl
+ * # HomeCtrl
  * Controller of the rwsprojectApp
  */
 angular.module('rwsprojectApp')
-    .controller('MainCtrl', function ($scope) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
+    .controller('HomeCtrl', function ($scope) {
+        $scope.rock = 'and roll';
 
         // this should use $http and ask the server if the user is logged in
         // what happens when their session times out?
