05e81841f3f887cee57d5f0390b95a2649019043
Christian Parker <christian.parker@emc.com>
2014-08-21 19:51:58 -0600

    python files

---
+++ server/src/HelloDB.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost",  # your host, usually localhost
+                     user="root",  # your username
+                     passwd="",  # your password
+                     db="rwsproject")  # name of the data base
+
+# you must create a Cursor object. It will let
+# you execute all the queries you need
+cur = db.cursor()
+
+# Use all the SQL you like
+cur.execute("SELECT * FROM accounts")
+
+# print all the first cell of all the rows
+for row in cur.fetchall():
+    print row[0]
+    print row[1]
