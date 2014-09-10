66dd7c66050377887e18982a8106622ea08eeb93
Christian Parker <christian.parker@emc.com>
2014-09-09 19:22:51 -0600

    attempt to keep mysql connection open

--- server/src/HelloFlask.py
+++ server/src/HelloFlask.py
@@ -11,10 +11,16 @@ app = Flask(__name__)
 
 app.permanent_session_lifetime = timedelta(minutes=60)
 
-db = MySQLdb.connect(host="localhost",  # your host, usually localhost
-                     user="root",  # your username
-                     passwd="smartlights",  # your password
-                     db="rwsproject")  # name of the data base
+db = None
+
+
+def createConnection():
+    db = MySQLdb.connect(host="localhost",  # your host, usually localhost
+                         user="root",  # your username
+                         passwd="smartlights",  # your password
+                         db="rwsproject")  # name of the data base
+    return db
+
 
 @app.route('/')
 def index():
@@ -28,35 +34,41 @@ def login():
     session.permanent = True
     if request.method == 'POST':
         print('received ' + str(request.json))
+        db.ping()
         cur = db.cursor(cursors.DictCursor)
         cur.execute("select * from accounts where user='{name}'".format(name=request.json['username']))
         user = cur.fetchone()
         if user['password'] == request.json['password']:
             session['username'] = request.json['username']
-            return '',200
+            cur.close()
+            return '', 200
         else:
-            return '',401
+            cur.close()
+            return '', 401
 
 
 @app.route('/server/checkAccess')
 def checkAccess():
     if 'username' in session:
-        return '',200
+        return '', 200
     else:
-        return '',401
+        return '', 401
+
 
 @app.route('/server/hello')
 def hello():
-    return '',200
+    return '', 200
 
 
 @app.route('/simplepost', methods=['POST'])
 def simple():
-    return '',200
+    return '', 200
+
 
 app.secret_key = 'slaskdjfalksdfs90df8sdf8s0d98f0sdf'
 
-
 if __name__ == '__main__':
+    db = createConnection()
+
     app.debug = True
     app.run(host='0.0.0.0')
