5164edf55b61e4d5583bd5e74b6d75a7d11a98c0
Christian Parker <christian.parker@emc.com>
2014-11-09 16:47:44 -0700

    misc fixes

--- app/scripts/controllers/enginesController.js
+++ app/scripts/controllers/enginesController.js
@@ -4,8 +4,53 @@ angular.module('rwsprojectApp')
 
 
             var channelsPerEngine = 47;
+            var enginesPerDirector = 41;
+
+            var cordOptions277 = [
+                'Included'
+            ];
+
+            var cordOptions250 = [
+                'US/Canada PC-NA-250',
+                'European Union PC-EU-120-250',
+                'UK, Hong Kong, Singapore PC-UK-120-250',
+                'Australia / New Zealand PC-AUS-120-250',
+                'Brazil PC-BRAZIL',
+                'China PC-CHINA',
+                'Denmark PC-DENMARK',
+                'Switzerland PC-SWISS',
+                'Excluded'
+            ];
+
+            $scope.disableStandardCord = false;
+            $scope.disableEmergencyCord = false;
 
             $rootScope.$on('entering4', function (event, fromTab, toTab) {
+
+                $scope.$watch('engineModel.voltageStandard', function(newVal, oldVal) {
+                    if(newVal == '277v') {
+                        $scope.disableStandardCord = true;
+                        $scope.cordOptions = cordOptions277;
+                        $scope.engineModel.cordStandard = 'Included'
+                    } else {
+                        $scope.disableStandardCord = false;
+                        $scope.cordOptions = cordOptions250;
+                    }
+                });
+
+                $scope.$watch('engineModel.voltageEmergency', function(newVal, oldVal) {
+                    if(newVal == '277v') {
+                        $scope.disableEmergencyCord = true;
+                        $scope.cordOptions = cordOptions277;
+                        $scope.engineModel.cordEmergency = 'Included'
+                    } else {
+                        $scope.disableEmergencyCord = false;
+                        $scope.cordOptions = cordOptions250;
+                    }
+                });
+
+                $scope.engineModel = $scope.engineModel ? $scope.engineModel : {};
+
                 $scope.standardChannels = _.reduce(dataService.fixtureLines, function (initial, fixtureRec) {
                     return initial + fixtureRec.channels.channel_count * parseInt(fixtureRec.standardQuantity);
                 }, 0);
@@ -16,6 +61,7 @@ angular.module('rwsprojectApp')
 
                 $scope.standardEngines = Math.ceil($scope.standardChannels / channelsPerEngine);
                 $scope.emergencyEngines = Math.ceil($scope.emergencyChannels / channelsPerEngine);
+                $scope.engineModel.directorCount = Math.ceil(($scope.standardEngines + $scope.emergencyEngines) / enginesPerDirector);
 
                 $scope.engineModel = $scope.engineModel ? $scope.engineModel : {};
 
