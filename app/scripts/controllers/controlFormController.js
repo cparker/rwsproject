04798aa667981667158d4c9254bf8a0d136e94a2
Christian Parker <christian.parker@emc.com>
2014-11-30 14:59:08 -0700

    added dimmer adaptor plates to BOM summary, also made dimmer plate input always enabled

--- app/scripts/controllers/controlFormController.js
+++ app/scripts/controllers/controlFormController.js
@@ -1,45 +1,46 @@
 angular.module('rwsprojectApp')
-    .controller('controlFormController', ['$scope', '$filter', '$rootScope', 'dataService', '$httpBackend',
-        function ($scope, $filter, $rootScope, dataService, $httpBackend) {
+  .controller('controlFormController', ['$scope', '$filter', '$rootScope', 'dataService', '$httpBackend',
+    function ($scope, $filter, $rootScope, dataService, $httpBackend) {
 
-            $rootScope.$on('entering3', function (event, fromTab, toTab) {
-                event.stopPropagation();
-                // defaults for non required fields
-                $scope.controlModel = $scope.controlModel ? $scope.controlModel : {};
+      $rootScope.$on('entering3', function (event, fromTab, toTab) {
+        event.stopPropagation();
+        // defaults for non required fields
+        $scope.controlModel = $scope.controlModel ? $scope.controlModel : {};
 
-                $scope.controlModel.sceneControllers = $scope.controlModel.sceneControllers || 0;
-                $scope.controlModel.dimmers = $scope.controlModel.dimmers || 0;
-                $scope.controlModel.spdtSwitches = $scope.controlModel.spdtSwitches || 0;
-            });
-
-
-            $rootScope.$on('leaving3', function (event, fromTab, toTab) {
-                event.stopPropagation();
-
-                // back
-                if (parseInt(toTab) < 3) {
-                    $rootScope.tabs.activeTab = toTab;
-                } else {
-
-                    // forward
-                    if ($scope.controlForm.$valid) {
-
-                        $scope.totalChannels = _.reduce(dataService.fixtureLines, function (initial, fixtureRec) {
-                            return initial +
-                                fixtureRec.channels.channel_count * parseInt(fixtureRec.standardQuantity) +
-                                fixtureRec.channels.channel_count * parseInt(fixtureRec.emergencyQuantity)
-                        }, 0);
-
-                        $scope.controlModel = $scope.controlModel ? $scope.controlModel : {};
-                        $scope.controlModel.totalSharingCables = $scope.totalChannels;
-                        dataService.controlModel = $scope.controlModel;
-                        $rootScope.tabs.activeTab = toTab;
-                    } else {
-                        $scope.controlForm.$setSubmitted();
-                    }
-                }
-
-            });
+        $scope.controlModel.sceneControllers = $scope.controlModel.sceneControllers || 0;
+        $scope.controlModel.dimmers = $scope.controlModel.dimmers || 0;
+        $scope.controlModel.spdtSwitches = $scope.controlModel.spdtSwitches || 0;
+        $scope.controlModel.dimmerPlates = $scope.controlModel.dimmerPlates || 0;
+      });
 
 
-        }]);
+      $rootScope.$on('leaving3', function (event, fromTab, toTab) {
+        event.stopPropagation();
+
+        // back
+        if (parseInt(toTab) < 3) {
+          $rootScope.tabs.activeTab = toTab;
+        } else {
+
+          // forward
+          if ($scope.controlForm.$valid) {
+
+            $scope.totalChannels = _.reduce(dataService.fixtureLines, function (initial, fixtureRec) {
+              return initial +
+                fixtureRec.channels.channel_count * parseInt(fixtureRec.standardQuantity) +
+                fixtureRec.channels.channel_count * parseInt(fixtureRec.emergencyQuantity)
+            }, 0);
+
+            $scope.controlModel = $scope.controlModel ? $scope.controlModel : {};
+            $scope.controlModel.totalSharingCables = $scope.totalChannels;
+            dataService.controlModel = $scope.controlModel;
+            $rootScope.tabs.activeTab = toTab;
+          } else {
+            $scope.controlForm.$setSubmitted();
+          }
+        }
+
+      });
+
+
+    }]);
