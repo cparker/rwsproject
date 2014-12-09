56119a415795569945b939ba0d3e01c0908b1c3a
Christian Parker <christian.parker@emc.com>
2014-12-08 20:32:12 -0700

    fixed issue where emergency light control count was wrong

--- app/scripts/controllers/summaryController.js
+++ app/scripts/controllers/summaryController.js
@@ -1,35 +1,14 @@
 angular.module('rwsprojectApp')
-    .controller('summaryController', ['$scope', '$filter', '$rootScope', 'dataService', '$httpBackend',
-        function ($scope, $filter, $rootScope, dataService, $httpBackend) {
+  .controller('summaryController', ['$scope', '$filter', '$rootScope', 'dataService', '$httpBackend',
+    function ($scope, $filter, $rootScope, dataService, $httpBackend) {
 
-            $rootScope.$on('entering8', function (event, fromTab, toTab) {
-                event.stopPropagation();
+      $rootScope.$on('entering8', function (event, fromTab, toTab) {
+        event.stopPropagation();
+      });
 
-                // recompute every time you enter the tab
-                $scope.emergencyRelayCount = dataService.emergencyOption == 1 ? 1 : 0;
-                $scope.emergencyGatewayCount = dataService.emergencyOption == 2 ? 1 : 0;
+      $rootScope.$on('leaving8', function (event, fromTab, toTab) {
+        event.stopPropagation();
+        $rootScope.tabs.activeTab = toTab;
+      });
 
-                $scope.sensorThreeFixtures = _.filter(dataService.fixtureLines, function (fix) {
-                    return fix.controlMethod.name === "Sensor 3";
-                });
-
-                $scope.ledGatewayFixtures = _.filter(dataService.fixtureLines, function (fix) {
-                    return fix.controlMethod.name.toLowerCase().indexOf('led gateway') != -1;
-                });
-
-                $scope.s3BySensorPart = _.pairs(_.groupBy($scope.sensorThreeFixtures, function (fix) {
-                    return fix.partInfo.part_number;
-                }));
-
-                $scope.ledBySensorPart = _.pairs(_.groupBy($scope.ledGatewayFixtures , function (fix) {
-                    return fix.partInfo.part_number;
-                }));
-            });
-
-
-            $rootScope.$on('leaving8', function (event, fromTab, toTab) {
-                event.stopPropagation();
-                $rootScope.tabs.activeTab = toTab;
-            });
-
-        }]);
+    }]);
